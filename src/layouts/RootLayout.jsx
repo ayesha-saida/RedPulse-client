1c9505f304e1121edcb9a95cc4c08591c6b5b94c
ayesha-saida <ayeshasultana0305@gmail.com>
2025-12-07 18:56:06 +0600

    creating routes

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import { Outlet } from 'react-router'
+
+const RootLayout = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+       <Outlet></Outlet>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default RootLayout
---
+++ src/routes/Router.jsx
@@ -0,0 +1,47 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../layouts/RootLayout";
+import Home from "../Pages/Home/Home";
+import Dashboard from "../layouts/Dashboard";
+import AuthLayout from "../layouts/AuthLayout";
+import SignIn from "../Pages/Authentication/SignIn";
+import SignUp from "../Pages/Authentication/SignUp";
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout /> ,
+    children: [
+        {
+            path: 'home',
+            element: <Home></Home>,
+        }
+
+    ]
+  },
+  {
+    path: "/",
+    element:  <AuthLayout />,
+    children: [
+        {
+            path: 'Sign Up',
+            element: <SignIn> </SignIn> ,
+        },
+        {
+            path: 'Sign In',
+            element: <SignUp> </SignUp> ,
+        },
+
+    ]
+  },
+  {
+    path: "/",
+    element: <Dashboard /> ,
+    children: [
+        {
+           
+        }
+
+    ]
+  },
+]);
