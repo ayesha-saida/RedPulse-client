f4173fcab38c52f26ba598aabffeba9913192c0f
ayesha-saida <ayeshasultana0305@gmail.com>
2025-12-08 21:07:12 +0600

    json data loaded for blood group type, district and upazila in register form

--- src/Pages/Authentication/SignUp.jsx
+++ src/Pages/Authentication/SignUp.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router'
 
 const SignUp = () => {
+  const [bloodGroup, setBloodGroup] = useState([])
+  const [district, setDistrict] = useState([])
+  const [upazila, setUpazila] = useState([])
+
+
+  //load json data
+ useEffect( () => {
+  const loadJSON = async () => {
+ try{
+   const res1 = await fetch('/bloodgroup.json');
+   const res2 = await  fetch('/district.json');
+   const res3 = await  fetch('/upazila.json');
+
+   const json1 = await res1.json();
+   const json2 = await res2.json();
+   const json3 = await res3.json();
+
+   setBloodGroup(json1)
+   setDistrict(json2)
+   setUpazila(json3)
+
+    // console.log(json1)
+   // console.log(json2)
+  // console.log(json3)
+ } 
+   catch (error){
+     console.log(error)
+  }
+}
+   loadJSON()
+
+ }, [])
+
   return (
      <div className='w-11/12 mx-auto'>
         <h1 className='text-2xl text-center font-semibold pt-7'>Welcome! Please Register to Access Our Blood Donation Services</h1>
@@ -24,23 +57,27 @@ const SignUp = () => {
 
       {/*blood group selection field */}
   <label className="label">Blood Group</label>
-  <select defaultValue="select" className="select">
+  <select defaultValue="select" className="select">  
   <option disabled={true}> Select </option>
-  <option>Crimson</option>
+   { 
+  bloodGroup.map( (group,id) =>  <option key={id}> {group.blood_group} </option> )
+    }
 </select>
 
       {/*district group selection field */}
   <label className="label">District</label>
   <select defaultValue="select" className="select">
   <option disabled={true}> Select </option>
-  <option>Crimson</option>
+  { district.map((district,id )=> <option key={id}>{district.name}</option>)
+    }
 </select>
 
       {/*upazila group selection field */}
   <label className="label">Upazila</label>
   <select defaultValue="select" className="select">
   <option disabled={true}> Select </option>
-  <option>Crimson</option>
+  {  upazila.map( (upazila,id) => <option key={id}>{upazila.name}</option>)
+  }
 </select>
 
   <label className="label">Password</label>
