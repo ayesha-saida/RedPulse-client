84d21ceea9e4d4dc39e78d7d1575500f220d94b0
ayesha-saida <ayeshasultana0305@gmail.com>
2025-12-14 20:40:27 +0600

    user login successfully implemented

--- src/Pages/Authentication/SignIn.jsx
+++ src/Pages/Authentication/SignIn.jsx
@@ -1,21 +1,52 @@
-import React from 'react'
-import { Link } from 'react-router'
+import React, { useContext } from 'react'
+import { useForm } from 'react-hook-form'
+import { Link, useLocation, useNavigate } from 'react-router'
+import { AuthContext } from '../Context/AuthProvider'
 
 const SignIn = () => {
+    const {loginUser} = useContext(AuthContext)
+   const navigate = useNavigate()
+   const {register, handleSubmit, formState:{errors}} = useForm()
+   const location = useLocation()
+  //console.log('In login page', location)
+
+  const handleLogin = (data) => {
+    console.log('form data', data)
+
+     loginUser(data.email, data.password)
+     .then(result => {
+     console.log(result.user)
+    alert('login succesfull')
+     navigate(location.state || '/')
+
+    }).catch(error => {
+     console.log(error)
+    })
+  }
+
   return (
     <div className='card bg-base-100 w-full mx-auto max-w-sm  '>
 
      <h1 className='text-2xl text-center font-semibold pt-7 px-5'>Welcome to Red Pulse</h1>
     
     <div className='flex flex-col justify-center items-center py-5'>
-   <form className='card-body'>
+   <form className='card-body' onSubmit={handleSubmit(handleLogin)}>
     <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
 
+  {/*email */}
   <label className="label">Email</label>
-  <input type="email" className="input" placeholder="Email" />
+  <input type="email"  {...register('email', {required: true})}  className="input" placeholder="Email" />
 
+   {
+   errors.email?.type === 'required' && <span className='text-red-500'> Email is required</span>
+  }
+
+  {/*password*/}
   <label className="label">Password</label>
-  <input type="password" className="input" placeholder="Password" />
+  <input type="password" {...register('password', {required: true, 
+    minLength:6 })}  className="input" placeholder="Password" />
+
+   {errors.password?.type ==='minLength' && <span className='text-red-500'>Password must be 6 characters or longer</span>}
 
   <button className="btn bg-[#eb2c29] text-white  mt-4">Login</button>
 
