e0189e955e2a6262d1d2a90bef06d243b704068d
ayesha-saida <ayeshasultana0305@gmail.com>
2026-01-30 23:33:24 +0600

    volunteer dashboard implemented

--- src/Pages/Dashboard/Admin Dashboard/AdminDashboardHome.jsx
+++ src/Pages/Dashboard/Admin Dashboard/AdminDashboardHome.jsx
@@ -1,8 +1,66 @@
 import React from 'react'
+import { Card, Row, Col, Statistic } from "antd"
+import {
+  UserOutlined,
+  DollarOutlined,
+  HeartOutlined,
+} from "@ant-design/icons"
 
 const AdminDashboardHome = () => {
-  return (
-    <div>Admin Dashboard Home</div>
+  
+  return(
+    <div className='max-w-7xl mx-auto p-4 sm:p-6 lg:p-8'>
+     <Row gutter={[16, 16]}>
+
+       {/* Card - 1 Donors */}
+    {/* Desktop: 8 (3 cards) | Tablet: 12 (2 cards) | Mobile: 24 (1 card) */}
+      <Col xs={24} sm={12} md={8}>
+        <Card
+          style={{
+            background: "linear-gradient(120deg, #fca5a5, #ffffff)",
+          }}>
+          <Statistic
+            title="Total Donors"
+            value={1280}
+            prefix={<UserOutlined style={{ color: "#991b1b" }} />}
+             styles={{ content: { color: "#450a0a" } }}
+          />
+        </Card>
+      </Col>
+
+         {/* Card - 2 Funding */}
+       <Col  xs={24} sm={12} md={8}>
+        <Card
+          style={{
+            background: "linear-gradient(180deg, #fca5a5, #ffffff)",
+          }} >
+          <Statistic
+            title="Total Funding"
+            value={45230}
+            prefix={<DollarOutlined style={{ color: "#991b1b" }} />}
+             styles={{ content: { color: "#450a0a" } }}
+            suffix="USD"
+          />
+        </Card>
+      </Col>
+
+        {/* Card - 3 Requests */}
+       <Col  xs={24} sm={12} md={8}>
+        <Card
+          style={{
+            background: "linear-gradient(135deg, #fca5a5, #ffffff)",
+          }}>
+          <Statistic
+            title="Blood Donation Requests"
+            value={342}
+            prefix={<HeartOutlined style={{ color: "#991b1b" }} />}
+             styles={{ content: { color: "#450a0a" } }}
+          />
+        </Card>
+      </Col>
+    </Row>
+
+    </div>
   )
 }
 
--- src/Pages/Dashboard/Volunteer Dashboard/VolunteerDashboardHome.jsx
+++ src/Pages/Dashboard/Volunteer Dashboard/VolunteerDashboardHome.jsx
@@ -1,8 +1,70 @@
 import React from 'react'
+import { Card, Row, Col, Statistic, Skeleton } from "antd"
+import {
+  UserOutlined,
+  HeartOutlined,
+} from "@ant-design/icons"
+import { useQuery } from '@tanstack/react-query'
+import useAxiosSecure from '../../../Hooks/useAxiosSecure'
 
 const VolunteerDashboardHome = () => {
-  return (
-    <div>Volunteer Dashboard Home</div>
+    const axiosSecure = useAxiosSecure()
+     const {data: states = {}, isLoading: statsLoading } = useQuery({
+          queryKey: ['stats'],
+          queryFn: async() => {
+            const res = await  axiosSecure.get(`/stats`)
+              return res.data
+          }
+         })
+
+    const {data: donations = [], isLoading: donationsLoading } = useQuery({
+         queryKey: ['donation'],
+         queryFn: async() => {
+           const res = await 
+           axiosSecure.get(`/donations`)
+             return res.data
+         }
+        })
+
+  return(
+    <div className='max-w-7xl mx-auto p-4 sm:p-6 lg:p-8'>
+     <Row gutter={[16, 16]}>
+
+       {/* Card - 1 Donors */}
+    {/* Desktop: 8 (3 cards) | Tablet: 12 (2 cards) | Mobile: 24 (1 card) */}
+       <Col  xs={24} sm={12} md={8}>
+        <Card
+          style={{
+           background: "linear-gradient(120deg, #fca5a5, #ffffff)" }}>
+          <Skeleton loading={statsLoading} active>
+          <Statistic
+            title="Total Donors"
+            value={statsLoading ? 0 : states.totalDonors || 0}
+            prefix={<UserOutlined style={{ color: "#991b1b" }} />}
+             styles={{ content: { color: "#450a0a" } }}
+          />
+          </Skeleton>
+        </Card>
+      </Col>
+
+        {/* Card - 3 Requests */}
+       <Col  xs={24} sm={12} md={8}>
+        <Card
+          style={{
+            background: "linear-gradient(135deg, #fca5a5, #ffffff)" }}>
+          <Skeleton loading={statsLoading} active>
+          <Statistic
+            title="Blood Donation Requests"
+            value={donationsLoading ? 0 : donations.length}
+            prefix={<HeartOutlined style={{ color: "#991b1b" }} />}
+             styles={{ content: { color: "#450a0a" } }}
+          />
+          </Skeleton>
+        </Card>
+      </Col>
+    </Row>
+
+    </div>
   )
 }
 
